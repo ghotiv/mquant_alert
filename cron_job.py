8a2b80d63cf4840006c1da59d94012d5fd457274
ghoti <ghotiv@gmail.com>
2021-06-18 10:57:58 +0800

    ghoti

--- cron_job.py
+++ cron_job.py
@@ -9,7 +9,7 @@ time_now = arrow.utcnow().to(TZ).format('YYYY-MM-DD HH:mm:ss')
 
 url = 'http://8.211.165.131:8008/get_twap_gap?twap_duration=60'
 r = requests.get(url)
-print(time_now,r.json())
+print(time_now,5*'',r.json())
 
 
 
